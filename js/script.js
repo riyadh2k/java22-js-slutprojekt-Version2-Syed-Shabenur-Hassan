fb1a1bf636c7d6a253527c748ceeae8fe7e809b2
Syed Shabenur Hassan <112406985+riyadh2k@users.noreply.github.com>
2023-02-16 23:05:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 const imageContainer = document.querySelector('.imageContainer');
 
-document.querySelector('.searchButton').addEventListener('click', getPhotosInfo);
+document.querySelector('button').addEventListener('click', getPhotosInfo);
 
+// Gets user input to GetPictures or if field is empty ,it will gives an alert
 function getPhotosInfo(event) {
     event.preventDefault();
 
     const textInput = document.querySelector('.nameInput').value;
     const numberInput = document.querySelector('.numberInput').value;
 
-    // console.log(textInput);
-    // console.log(numberInput);
 
     if (textInput != '' && numberInput != '' && numberInput > 0) {
         imageContainer.innerHTML = '';
         getPhotoImages(textInput, numberInput);
     } else {
-        //imageContainer.innerHTML = '';
         alert('Name of Photos and Number cannot be Empty. Number should be above 0');
     }
 }
 
-function getPhotoImages(nameInput, numberInput,sortImages) {
+// Download Data from flickr and if anything goes wrong it will display error
+function getPhotoImages(nameInput, numberInput, sortImages) {
     const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9ee80ef9e0c728929d3fd608f5f3cad1&tags=${nameInput}&sort=${sortImages}&per_page=${numberInput}&format=json&nojsoncallback=1`;
 
     //console.log(url);
@@ -46,9 +45,9 @@ function getPhotoImages(nameInput, numberInput,sortImages) {
         });
 }
 
-
+// For each query get its necessary data and display the Image. And If no image is available then it will display no result
 function displayPhotoImages(photoInfo) {
-    const sizeInput = document.querySelector('.dropDown').value;
+    const sizeInput = document.querySelector('.picSize').value;
     //console.log(sizeInput);
     //console.log(photoInfo);
 
@@ -58,13 +57,13 @@ function displayPhotoImages(photoInfo) {
 
             const imgUrl2 = `https://live.staticflickr.com/${imgUrl.server}/${imgUrl.id}_${imgUrl.secret}_${sizeInput}.jpg;`;
 
-            const blankLink= document.createElement('a');
+            const blankLink = document.createElement('a');
             const img = document.createElement('img');
             imageContainer.append(blankLink);
             blankLink.append(img);
             img.src = imgUrl2;
-            blankLink.href=imgUrl2;
-            blankLink.target= '_blank';
+            blankLink.href = imgUrl2;
+            blankLink.target = '_blank';
         });
 
     else {
